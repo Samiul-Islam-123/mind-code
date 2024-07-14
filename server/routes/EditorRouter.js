58c2954eb0a6dd7eec0eb53f13cb68e1cbfe7208
MD Samiul Islam <isamiul099@gmail.com>
2024-07-15 02:37:19 +0530

    CHanges

--- server/routes/EditorRouter.js
+++ server/routes/EditorRouter.js
@@ -1,4 +1,4 @@
-const { ReadfileContents, SaveCurrentFile, CreateNewFile, CreateNewFolderController, DeleteFileOrFolderController, RenameFileOrFolderController } = require("../controllers/EditorController");
+const { ReadfileContents, SaveCurrentFile, CreateNewFile, CreateNewFolderController } = require("../controllers/EditorController");
 
 const EditorRouter = require("express").Router();
 
@@ -6,8 +6,7 @@ EditorRouter.get('/:filePath/:clerkID', ReadfileContents);
 EditorRouter.post('/save-file', SaveCurrentFile)
 EditorRouter.post('/new-file', CreateNewFile)
 EditorRouter.post('/new-folder', CreateNewFolderController),
-EditorRouter.delete('/delete', DeleteFileOrFolderController);
-EditorRouter.put('/rename', RenameFileOrFolderController)
+
 
 
 module.exports = EditorRouter;
