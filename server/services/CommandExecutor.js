487a7d92537639ec4cec567ba3dd044adaa48635
MD Samiul Islam <isamiul099@gmail.com>
2024-07-17 16:00:52 +0530

    terminal changes 5

--- server/services/CommandExecutor.js
+++ server/services/CommandExecutor.js
@@ -1,30 +1,16 @@
 const shell = require('shelljs');
-const fs = require('fs-extra');
-const path = require('path');
 
 const runCommand = (cmd) => {
-  return new Promise(async (resolve, reject) => {
-    const touchMatch = cmd.match(/^touch\s+(.*)$/);
-
-    if (touchMatch) {
-      const filePath = touchMatch[1];
-
-      try {
-        await fs.outputFile(filePath, path.basename(filePath));
-        resolve(`File ${filePath} created and written to successfully.`);
-      } catch (error) {
-        reject(new Error(`Error writing to file: ${error.message}`));
+  return new Promise((resolve, reject) => {
+    shell.exec(cmd, (code, stdout, stderr) => {
+      if (code !== 0) {
+        reject(new Error(stderr));
+      } else {
+        resolve(stdout);
       }
-    } else {
-      shell.exec(cmd, (code, stdout, stderr) => {
-        if (code !== 0) {
-          reject(new Error(stderr));
-        } else {
-          resolve(stdout);
-        }
-      });
-    }
+    });
   });
 };
+
 
 module.exports = runCommand;
